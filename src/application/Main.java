19e52466137a5a1f5804383807d97ecfefe91fd6
Roger <rguinartv@inscastellet.cat>
2022-05-19 19:44:13 +0200

    Començant la Interface gràfica

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,35 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+			Scene scene = new Scene(root,640,480);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			root.addEventFilter(MouseEvent.MOUSE_CLICKED, event -> System.out.println(event.getTarget()));
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
+	}
+	public void hoverlogin () {
+		System.out.println("test");
 	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,7 @@
 module AlienTrolls {
 	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
